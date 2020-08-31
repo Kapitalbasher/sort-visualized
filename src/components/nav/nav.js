6ea6a5776aa32e51e99ca33511ccdd0acc4ea6ce
Kapitalbasher <timschulz560@gmail.com>
2020-08-31 22:28:57 +0200

    created nav component without much functionality yet

---
+++ src/components/nav/nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./nav.css";
+const Nav = () => {
+  return (
+    <section className="nav-container">
+      {/* <header className="nav-logo">
+        <a href="#"> Sorting</a>
+      </header> */}
+      <ul>
+        <li>Algorithm</li>
+        <li>Settings</li>
+        <li>About</li>
+      </ul>
+    </section>
+  );
+};
+export default Nav;
