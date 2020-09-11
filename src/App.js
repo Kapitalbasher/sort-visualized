8ca7da7158f6dfebd9ea5e9c1fc4459648c76eeb
Kapitalbasher <timschulz560@gmail.com>
2020-09-11 21:56:09 +0200

    fixed overflow

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 //Component Imports
 import Nav from "./components/nav/nav.js";
+import { getColumns } from "./components/columns/column/column.js";
 import Columns from "./components/columns/columns.js";
+import Information from "./components/information/information.js";
+import Sort from "./components/columns/sort.js";
 
+let sort = new Sort();
 function App() {
+  const [columnList, setColumnList] = useState(getColumns(sort.width));
+  // new columns on resize
+  // React.useEffect(function setupListener() {
+  //   function handleResize() {
+  //     setColumnList(getColumns(columnWidth));
+  //   }
+  //   window.addEventListener("resize", handleResize);
+
+  //   return function cleanupListener() {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // });
   return (
     <div className="App">
-      <Nav />
-      <Columns />
+      <Nav sort={sort} columnList={columnList} setColumnList={setColumnList} />
+      <Columns columnList={columnList} setColumnList={setColumnList} />
+      <Information sort={sort} />
     </div>
   );
 }
-
 export default App;
