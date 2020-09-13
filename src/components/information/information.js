dfcc6255e6ab06bb0e6a992fdab5215a05c673d0
Kapitalbasher <timschulz560@gmail.com>
2020-09-13 19:25:20 +0200

    early crossfade attempt

--- src/components/information/information.js
+++ src/components/information/information.js
@@ -12,15 +12,27 @@ const Information = ({ sort }) => {
 
   sort.setInformation = setInformation;
   return (
-    <section className="info">
-      <div className="description">
-        <h3>Description:</h3>
-        <p>{information.description}</p>
+    <section className="info-container">
+      <div className="info">
+        <div className="description">
+          <h3>Description:</h3>
+          <p>{information.description}</p>
+        </div>
+        <div className="bigO">
+          <h3>Big O: </h3>
+          <p>{information.bigO}</p>
+        </div>
       </div>
-      <div className="bigO">
-        <h3>Big O: </h3>
-        <p>{information.bigO}</p>
-      </div>
+      {/* <div className="info-invisible">
+        <div className="description">
+          <h3>Description:</h3>
+          <p>{information.description}</p>
+        </div>
+        <div className="bigO">
+          <h3>Big O: </h3>
+          <p>{information.bigO}</p>
+        </div>
+      </div> */}
     </section>
   );
 };
