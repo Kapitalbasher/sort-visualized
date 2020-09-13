f2322086ecc4e3cba1f89cba1d23b25aaac8d039
Kapitalbasher <timschulz560@gmail.com>
2020-09-13 19:22:13 +0200

    added checkboxes for freezing and looping

--- src/components/nav/dropdown/settings/settings.js
+++ src/components/nav/dropdown/settings/settings.js
@@ -2,6 +2,8 @@ import React from "react";
 import { getColumns } from "../../../columns/column/column.js";
 import Slider from "@material-ui/core/Slider";
 import Input from "@material-ui/core/Input";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 import { makeStyles } from "@material-ui/core/styles";
 import "./settings.css";
 
@@ -13,35 +15,44 @@ const useStyles = makeStyles({
     width: "30%",
     marginLeft: 10,
   },
+  mobile: {
+    width: "25vw",
+  },
 });
 
 const Settings = (props) => {
   const classes = useStyles();
   const sort = props.sort;
-  const marksSpeed = [
-    {
-      value: 0.25,
-      label: "slow",
-    },
-    {
-      value: 100,
-      label: "fast",
-    },
-  ];
-  const marksWidth = [
-    {
-      value: 3,
-      label: "small",
-    },
-    {
-      value: 150,
-      label: "big",
-    },
-  ];
+  let labelSize = { fontSize: "calc(15px + 0.75vw)" };
+  let marksSpeed = [];
+  let marksWidth = [];
+  if (window.innerHeight < window.innerWidth) {
+    labelSize.fontSize = "calc(10px + 0.75vw)";
+    marksSpeed = [
+      {
+        value: 0.25,
+        label: "slow",
+      },
+      {
+        value: 100,
+        label: "fast",
+      },
+    ];
+    marksWidth = [
+      {
+        value: 3,
+        label: "small",
+      },
+      {
+        value: 150,
+        label: "big",
+      },
+    ];
+  }
   return (
     <div className={props.classes}>
       <form
-        className={"form-speed " + classes.root}
+        className={"form-speed " + classes.root + classes.mobile}
         onSubmit={(e) => {
           e.preventDefault();
         }}
@@ -85,36 +96,72 @@ const Settings = (props) => {
           </div>
         </div>
       </form>
-      <button
-        onClick={() => {
-          if (sort.sorting === false) return;
-          if (sort.animations[0].playState === "paused") {
-            sort.continueAnimations();
-          } else {
-            sort.pauseAnimations();
-          }
-          props.setFreezed(!props.freezed);
-        }}
+      <FormControlLabel
+        value="Freeze"
+        control={
+          <Checkbox
+            disabled={!sort.sorting}
+            checked={props.freezed}
+            value={"Freeze"}
+            onClick={() => {
+              if (sort.sorting === false) return;
+              if (sort.animations[0] === undefined) {
+                if (sort.freezeCallback !== undefined) {
+                  sort.freezeCallback();
+                  sort.freezeCallback = undefined;
+                }
+              } else {
+                if (sort.animations[0].playState === "paused") {
+                  sort.continueAnimations();
+                } else {
+                  sort.pauseAnimations();
+                }
+              }
+              props.setFreezed(!props.freezed);
+            }}
+          />
+        }
+        label={<span style={{ labelSize }}>Freeze</span>}
+        labelPlacement="end"
       >
-        {props.freezed ? "Unfreeze" : "Freeze"}
-      </button>
-      {/* <button
-        onClick={() => {
-          sort.sorting = false;
-        }}
-      >
-        Stop
-      </button> */}
+        {" "}
+      </FormControlLabel>
+
+      <FormControlLabel
+        value="loop"
+        control={
+          <Checkbox
+            checked={props.loop}
+            value={"loop"}
+            onClick={() => {
+              props.setLoop(!sort.loop);
+            }}
+          />
+        }
+        label={<span style={labelSize}>Loop</span>}
+        labelPlacement="end"
+      ></FormControlLabel>
       <button
+        className="resort"
         onClick={async () => {
           sort.abruptStop();
           props.setColumnList(getColumns(sort.width));
           props.setFreezed(false);
+          document
+            .querySelector(".resort")
+            .animate(
+              [{ transform: `rotate(0)` }, { transform: `rotate(360deg)` }],
+              {
+                // timing options
+                duration: 500,
+                iterations: 1,
+                easing: "ease",
+              }
+            );
         }}
       >
         Resort
       </button>
-
       <form className={classes.root}>
         <label htmlFor="ColumnWidth">Width</label>
 
